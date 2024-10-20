f08c4013645ea59870c1641172414ab7edb63fbf
topi314 <git@topi.wtf>
2024-10-20 13:01:04 +0200

    add .editorconfig & reformat

--- src/main/java/com/github/topi314/sponsorblock/plugin/SegmentHandler.java
+++ src/main/java/com/github/topi314/sponsorblock/plugin/SegmentHandler.java
@@ -10,34 +10,34 @@ import java.util.List;
 
 public class SegmentHandler implements TrackMarkerHandler {
 
-    private final ISocketContext context;
-    private final long guildId;
-    private final AudioTrack track;
-    private final List<? extends TrackMarkable> segments;
-    private int currentSegment;
+	private final ISocketContext context;
+	private final long guildId;
+	private final AudioTrack track;
+	private final List<? extends TrackMarkable> segments;
+	private int currentSegment;
 
-    public SegmentHandler(ISocketContext context, long guildId, AudioTrack track, List<? extends TrackMarkable> segments) {
-        this.context = context;
-        this.guildId = guildId;
-        this.track = track;
-        this.segments = segments;
-    }
+	public SegmentHandler(ISocketContext context, long guildId, AudioTrack track, List<? extends TrackMarkable> segments) {
+		this.context = context;
+		this.guildId = guildId;
+		this.track = track;
+		this.segments = segments;
+	}
 
-    @Override
-    public void handle(MarkerState state) {
-        if (!(state == MarkerState.REACHED || state == MarkerState.LATE || state == MarkerState.BYPASSED)) {
-            return;
-        }
-        var markable = segments.get(this.currentSegment);
-        if (markable instanceof Chapter chapter) {
-            context.sendMessage(EventsKt.eventSerializer(), new ChapterStarted("event", String.valueOf(guildId), chapter));
-        } else if (markable instanceof Segment segment) {
-            track.setPosition(segment.getEnd());
-            context.sendMessage(EventsKt.eventSerializer(), new SegmentSkipped("event", String.valueOf(guildId), segment));
-        }
-        this.currentSegment++;
-        if (this.currentSegment < segments.size()) {
-            track.setMarker(new TrackMarker(segments.get(this.currentSegment).getStart(), this));
-        }
-    }
+	@Override
+	public void handle(MarkerState state) {
+		if (!(state == MarkerState.REACHED || state == MarkerState.LATE || state == MarkerState.BYPASSED)) {
+			return;
+		}
+		var markable = segments.get(this.currentSegment);
+		if (markable instanceof Chapter chapter) {
+			context.sendMessage(EventsKt.eventSerializer(), new ChapterStarted("event", String.valueOf(guildId), chapter));
+		} else if (markable instanceof Segment segment) {
+			track.setPosition(segment.getEnd());
+			context.sendMessage(EventsKt.eventSerializer(), new SegmentSkipped("event", String.valueOf(guildId), segment));
+		}
+		this.currentSegment++;
+		if (this.currentSegment < segments.size()) {
+			track.setMarker(new TrackMarker(segments.get(this.currentSegment).getStart(), this));
+		}
+	}
 }
--- src/main/java/com/github/topi314/sponsorblock/plugin/SponsorBlockPlugin.java
+++ src/main/java/com/github/topi314/sponsorblock/plugin/SponsorBlockPlugin.java
@@ -31,143 +31,143 @@ import java.util.stream.Collectors;
 @RestController
 public class SponsorBlockPlugin extends PluginEventHandler {
 
-	private static final Logger log = LoggerFactory.getLogger(SponsorBlockPlugin.class);
-	private static final String SPONSORBLOCK_URL = "https://sponsor.ajay.app/api/skipSegments?videoID=%s&categories=%s";
+    private static final Logger log = LoggerFactory.getLogger(SponsorBlockPlugin.class);
+    private static final String SPONSORBLOCK_URL = "https://sponsor.ajay.app/api/skipSegments?videoID=%s&categories=%s";
 
-	private final HttpInterfaceManager httpInterfaceManager;
-	private final Map<String, Map<Long, Set<String>>> categoriesToSkip;
+    private final HttpInterfaceManager httpInterfaceManager;
+    private final Map<String, Map<Long, Set<String>>> categoriesToSkip;
 
-	public SponsorBlockPlugin() {
-		log.info("Loading SponsorBlock Plugin...");
-		this.httpInterfaceManager = HttpClientTools.createDefaultThreadLocalManager();
-		this.categoriesToSkip = new ConcurrentHashMap<>();
-	}
+    public SponsorBlockPlugin() {
+        log.info("Loading SponsorBlock Plugin...");
+        this.httpInterfaceManager = HttpClientTools.createDefaultThreadLocalManager();
+        this.categoriesToSkip = new ConcurrentHashMap<>();
+    }
 
-	public Map<Long, Set<String>> getCategoriesToSkip(String sessionId) {
-		return this.categoriesToSkip.get(sessionId);
-	}
+    public Map<Long, Set<String>> getCategoriesToSkip(String sessionId) {
+        return this.categoriesToSkip.get(sessionId);
+    }
 
-	@GetMapping("/v4/sessions/{sessionId}/players/{guildId}/sponsorblock/categories")
-	public Set<String> getCategoriesToSkip(@PathVariable String sessionId, @PathVariable long guildId) {
-		var guildCategoriesToSkip = this.categoriesToSkip.get(sessionId);
-		if (guildCategoriesToSkip == null) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Session not found");
-		}
-		var categoriesToSkip = guildCategoriesToSkip.get(guildId);
-		if (categoriesToSkip == null) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Guild not found");
-		}
-		return categoriesToSkip;
-	}
+    @GetMapping("/v4/sessions/{sessionId}/players/{guildId}/sponsorblock/categories")
+    public Set<String> getCategoriesToSkip(@PathVariable String sessionId, @PathVariable long guildId) {
+        var guildCategoriesToSkip = this.categoriesToSkip.get(sessionId);
+        if (guildCategoriesToSkip == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Session not found");
+        }
+        var categoriesToSkip = guildCategoriesToSkip.get(guildId);
+        if (categoriesToSkip == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Guild not found");
+        }
+        return categoriesToSkip;
+    }
 
-	@PutMapping("/v4/sessions/{sessionId}/players/{guildId}/sponsorblock/categories")
-	public void setCategoriesToSkip(@PathVariable String sessionId, @PathVariable long guildId, @RequestBody Set<String> categories) {
-		var guildCategoriesToSkip = this.categoriesToSkip.get(sessionId);
-		if (guildCategoriesToSkip == null) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Session not found");
-		}
-		guildCategoriesToSkip.put(guildId, categories);
-	}
+    @PutMapping("/v4/sessions/{sessionId}/players/{guildId}/sponsorblock/categories")
+    public void setCategoriesToSkip(@PathVariable String sessionId, @PathVariable long guildId, @RequestBody Set<String> categories) {
+        var guildCategoriesToSkip = this.categoriesToSkip.get(sessionId);
+        if (guildCategoriesToSkip == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Session not found");
+        }
+        guildCategoriesToSkip.put(guildId, categories);
+    }
 
-	@DeleteMapping("/v4/sessions/{sessionId}/players/{guildId}/sponsorblock/categories")
-	public void removeCategoriesToSkip(@PathVariable String sessionId, @PathVariable long guildId) {
-		var guildCategoriesToSkip = this.categoriesToSkip.get(sessionId);
-		if (guildCategoriesToSkip == null) {
-			return;
-		}
-		guildCategoriesToSkip.remove(guildId);
-	}
+    @DeleteMapping("/v4/sessions/{sessionId}/players/{guildId}/sponsorblock/categories")
+    public void removeCategoriesToSkip(@PathVariable String sessionId, @PathVariable long guildId) {
+        var guildCategoriesToSkip = this.categoriesToSkip.get(sessionId);
+        if (guildCategoriesToSkip == null) {
+            return;
+        }
+        guildCategoriesToSkip.remove(guildId);
+    }
 
-	@Override
-	public void onWebSocketOpen(ISocketContext context, boolean resumed) {
-		this.categoriesToSkip.put(context.getSessionId(), new ConcurrentHashMap<>());
-	}
+    @Override
+    public void onWebSocketOpen(ISocketContext context, boolean resumed) {
+        this.categoriesToSkip.put(context.getSessionId(), new ConcurrentHashMap<>());
+    }
 
-	@Override
-	public void onSocketContextDestroyed(ISocketContext context) {
-		this.categoriesToSkip.remove(context.getSessionId());
-	}
+    @Override
+    public void onSocketContextDestroyed(ISocketContext context) {
+        this.categoriesToSkip.remove(context.getSessionId());
+    }
 
-	@Override
-	public void onNewPlayer(@NotNull ISocketContext context, IPlayer iPlayer) {
-		iPlayer.getAudioPlayer().addListener(new PlayerListener(this, context, iPlayer.getGuildId()));
-	}
+    @Override
+    public void onNewPlayer(@NotNull ISocketContext context, IPlayer iPlayer) {
+        iPlayer.getAudioPlayer().addListener(new PlayerListener(this, context, iPlayer.getGuildId()));
+    }
 
-	@Override
-	public void onDestroyPlayer(ISocketContext context, @NotNull IPlayer player) {
-		var guildCategoriesToSkip = categoriesToSkip.get(context.getSessionId());
-		if (guildCategoriesToSkip != null) {
-			guildCategoriesToSkip.remove(player.getGuildId());
-		}
-	}
+    @Override
+    public void onDestroyPlayer(ISocketContext context, @NotNull IPlayer player) {
+        var guildCategoriesToSkip = categoriesToSkip.get(context.getSessionId());
+        if (guildCategoriesToSkip != null) {
+            guildCategoriesToSkip.remove(player.getGuildId());
+        }
+    }
 
-	public List<Segment> retrieveVideoSegments(String videoId, Set<String> categories) throws IOException {
-		var queryParam = URLEncoder.encode("[" + categories.stream().map(s -> "\"" + s + "\"").collect(Collectors.joining(", ")) + "]", StandardCharsets.UTF_8);
-		var request = new HttpGet(String.format(SPONSORBLOCK_URL, videoId, queryParam));
+    public List<Segment> retrieveVideoSegments(String videoId, Set<String> categories) throws IOException {
+        var queryParam = URLEncoder.encode("[" + categories.stream().map(s -> "\"" + s + "\"").collect(Collectors.joining(", ")) + "]", StandardCharsets.UTF_8);
+        var request = new HttpGet(String.format(SPONSORBLOCK_URL, videoId, queryParam));
 
-		var json = HttpClientTools.fetchResponseAsJson(this.httpInterfaceManager.getInterface(), request);
-		if (json == null) {
-			return List.of();
-		}
+        var json = HttpClientTools.fetchResponseAsJson(this.httpInterfaceManager.getInterface(), request);
+        if (json == null) {
+            return List.of();
+        }
 
-		var segments = new ArrayList<Segment>();
-		for (var segment : json.values()) {
-			var segmentTimes = segment.get("segment");
-			segments.add(new Segment(segment.get("category").text(), (long) (segmentTimes.index(0).as(Float.class) * 1000), (long) (segmentTimes.index(1).as(Float.class) * 1000)));
-		}
-		return segments;
-	}
+        var segments = new ArrayList<Segment>();
+        for (var segment : json.values()) {
+            var segmentTimes = segment.get("segment");
+            segments.add(new Segment(segment.get("category").text(), (long) (segmentTimes.index(0).as(Float.class) * 1000), (long) (segmentTimes.index(1).as(Float.class) * 1000)));
+        }
+        return segments;
+    }
 
-	public static class PlayerListener extends AudioEventAdapter {
+    public static class PlayerListener extends AudioEventAdapter {
 
-		private final SponsorBlockPlugin plugin;
-		private final ISocketContext context;
-		private final long guildId;
+        private final SponsorBlockPlugin plugin;
+        private final ISocketContext context;
+        private final long guildId;
 
-		public PlayerListener(SponsorBlockPlugin plugin, ISocketContext socketContext, long guildId) {
-			this.plugin = plugin;
-			this.context = socketContext;
-			this.guildId = guildId;
-		}
+        public PlayerListener(SponsorBlockPlugin plugin, ISocketContext socketContext, long guildId) {
+            this.plugin = plugin;
+            this.context = socketContext;
+            this.guildId = guildId;
+        }
 
-		@Override
-		public void onTrackStart(AudioPlayer player, AudioTrack track) {
-			if (track.getSourceManager() == null || !track.getSourceManager().getSourceName().equals("youtube")) {
-				return;
-			}
-			var guildCategoriesToSkip = this.plugin.getCategoriesToSkip(this.context.getSessionId());
-			if (guildCategoriesToSkip == null) {
-				return;
-			}
-			var categories = guildCategoriesToSkip.get(this.guildId);
-			if (categories == null) {
-				return;
-			}
-			List<TrackMarkable> markables;
-			try (var httpInterface = plugin.httpInterfaceManager.getInterface()) {
-				var segments = this.plugin.retrieveVideoSegments(track.getIdentifier(), categories);
-				if (!segments.isEmpty()) {
-					context.sendMessage(EventsKt.eventSerializer(), new SegmentsLoaded("event", String.valueOf(guildId), segments));
-				}
-				var chapters = InnerTubeClient.requestVideoChaptersById(httpInterface, track.getIdentifier(), track.getDuration());
-				if (!chapters.isEmpty()) {
-					context.sendMessage(EventsKt.eventSerializer(), new ChaptersLoaded("event", String.valueOf(guildId), chapters));
-				}
-				var output = new ArrayList<TrackMarkable>(segments);
-				output.addAll(chapters);
-				markables = output;
+        @Override
+        public void onTrackStart(AudioPlayer player, AudioTrack track) {
+            if (track.getSourceManager() == null || !track.getSourceManager().getSourceName().equals("youtube")) {
+                return;
+            }
+            var guildCategoriesToSkip = this.plugin.getCategoriesToSkip(this.context.getSessionId());
+            if (guildCategoriesToSkip == null) {
+                return;
+            }
+            var categories = guildCategoriesToSkip.get(this.guildId);
+            if (categories == null) {
+                return;
+            }
+            List<TrackMarkable> markables;
+            try (var httpInterface = plugin.httpInterfaceManager.getInterface()) {
+                var segments = this.plugin.retrieveVideoSegments(track.getIdentifier(), categories);
+                if (!segments.isEmpty()) {
+                    context.sendMessage(EventsKt.eventSerializer(), new SegmentsLoaded("event", String.valueOf(guildId), segments));
+                }
+                var chapters = InnerTubeClient.requestVideoChaptersById(httpInterface, track.getIdentifier(), track.getDuration());
+                if (!chapters.isEmpty()) {
+                    context.sendMessage(EventsKt.eventSerializer(), new ChaptersLoaded("event", String.valueOf(guildId), chapters));
+                }
+                var output = new ArrayList<TrackMarkable>(segments);
+                output.addAll(chapters);
+                markables = output;
 
-				output.sort(Comparator.comparing(TrackMarkable::getStart));
-			} catch (IOException e) {
-				log.error("Failed to retrieve video segments", e);
-				return;
-			}
-			log.info("Categories are: {}", markables);
-			if (!markables.isEmpty()) {
-				track.setMarker(new TrackMarker(markables.get(0).getStart(), new SegmentHandler(context, this.guildId, track, markables)));
-			}
-		}
+                output.sort(Comparator.comparing(TrackMarkable::getStart));
+            } catch (IOException e) {
+                log.error("Failed to retrieve video segments", e);
+                return;
+            }
+            log.info("Categories are: {}", markables);
+            if (!markables.isEmpty()) {
+                track.setMarker(new TrackMarker(markables.get(0).getStart(), new SegmentHandler(context, this.guildId, track, markables)));
+            }
+        }
 
-	}
+    }
 
 }
